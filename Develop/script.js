9b26f23a480089b9a64410779e94e6ac7bc0bb20
Nicholas Vrtis <nvav91@gmail.com>
2020-11-21 19:34:01 +0100

    managed to get all the arrays to work

--- Develop/script.js
+++ Develop/script.js
@@ -9,12 +9,19 @@ var numericNumber = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 var specialChar = ['#', '$', '%', '&', '(', ')', '*', '+', ',', '-', '.', '/', ':', ';', '<', '=', '>', '?', '@', '['];
 
 // password generator
-function generatePassword() {
-  var charcount = parseInt(prompt("Please enter a number between 8 - 128", "8-128"))
+function getPwdLength() {
+  var charCount = parseInt(prompt("Please enter a number between 8 - 128", "8-128"));
 // limits users input on password length
-if(charcount >7 && charcount <= 128) {
+if (isNaN(charCount)===true){
+alert("Please input a number!")};
+if(charCount <= 7 && charCount > 128) {
+  charCount = parseInt(prompt("Please enter a number between 8 - 128", "8-128"));
+}
+return charCount;};
+
+
+function getCharSelection() {
   // pushes all var the users chooses, into 1 var
-  var finalchars=[]
   // Criterias
   var includeLowerC = confirm ('Would you want lower case letter in your password?')
   var includeUpperC = confirm ('Would you want upper case letter in your password?')
@@ -22,72 +29,88 @@ if(charcount >7 && charcount <= 128) {
   var includesymbols = confirm('Would you want symbols in your password?')
 // need to include one criteria
 if (includeLowerC=== false && includeUpperC=== false && includenumber=== false && includesymbols === false) {
-  alert('You need to pick atleast one criteria \n Please try again')}
+  alert('You need to pick atleast one criteria \n Please try again')
+ return;}
 
-  // pushes the included criterias into the finalchars var
-  if (includeLowerC===true) {
+ var finalChars={
+   includeLowerC:includeLowerC,
+   includeUpperC:includeUpperC,
+   includenumber:includenumber,
+   includesymbols:includesymbols
+ };
+return finalChars;
+}
+
+function list(array) {
+  var picker=Math.floor(Math.random()*array.length);
+  var randomChar= array[picker];
+  return randomChar;
+}
+
+function getCharArray(finalChars) {
+  var allTheArrays = []
+  // pick the variable types
+  // array 
+  // if statments: includesymbols
+  // // pushes the included criterias into the finalchars var
+  if (finalChars.includeLowerC===true) {
     for (var i = 0;i < lowerCaseChar.length;i++)
-finalchars.push (lowerCaseChar[i])
+  allTheArrays.push (lowerCaseChar[i])
   }
-  if (includeUpperC===true) {
+  if (finalChars.includeUpperC===true) {
     for (var i = 0;i < upperCaseChar.length;i++)
-    finalchars.push (upperCaseChar[i])
+    allTheArrays.push (upperCaseChar[i])
   }
-  if (includenumber===true) {
+  if (finalChars.includenumber===true) {
     for (var i = 0;i < numericNumber.length;i++)
-finalchars.push (numericNumber[i])
+    allTheArrays.push (numericNumber[i])
   }
-  if (includesymbols===true) {
+  if (finalChars.includesymbols===true) {
     for (var i = 0;i < specialChar.length;i++)
-    finalchars.push (specialChar[i])
+    allTheArrays.push (specialChar[i])
   }
+  return allTheArrays;
+
+}
+console.log(getCharArray(getCharSelection()))
+
+function getPassword (charCount) {
+var finalpwd = ""
+  for(var i = 0;i < charCount.length;i++){
+    finalpwd += randomChar (getCharArray(getCharSelection()))
+    
+  }
+  
+  return finalpwd;
+}
+
+console.log(getPassword(getPwdLength()))
+
+// f
 // avoids getting arrarys inside of arrays
-  var flattenFinalChars = finalchars.flat()
+  // var flattenFinalChars = finalchars.flat()
 // getting password to become as string
-  var finalpassword = ''
+  // var finalpassword = ''
 // getting random number if the chosen criterias
-  for (var i = 0; charcount;++i) {
-    var randomIndex = Math.floor(Math.random() * flattenFinalChars.length)
-  }
-    var randomChar = flattenedFinalChars[randomIndex]
+  // for (var i = 0; charcount;++i) {
+  //   var randomIndex = Math.floor(Math.random() * flattenFinalChars.length)
+  // }
+  //   var randomChar = flattenedFinalChars[randomIndex]
 
-    finalpassword += randomChar
-    alert (finalpassword)
-}
-else {alert("Please input a number between 8-128")}
-}
-
-// need to figure out why it is crashing ( is the problem between line 45-55/62-67)
-// need to figure out how to limit users password to the length of the input
-// need to figure out how to make atleast 1 char from each chosen criteria to appear in the password
-// i believe line 69 will make the password variable appear as a text in the website, need to make sure
-
+  //   finalpassword += randomChar
+  //   alert (finalpassword)
 
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  // password = generatePassword.finalpassword
+  getPwdLength()
+  getCharSelection() 
+  var password = getPassword();
   var passwordText = document.querySelector("#password");
   passwordText.inntertext = password;
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-
-//  run 3
-
-// make a function that will run after click the generate password button that get the user to input the length of password (8-128)
-
-
-// when the user accept certain criterias, they will be randomly added atleast 1 character from each criteria
-
-// the password will then be written in the textbox on site
-
-// how to make the users input become the length of password??
-
-// make a function that will run the radomiser if the criteria is true?
-
-// make the input of the randomiser to randomise everything in a string which will shown in the text area/ make every character in the password
